68acda7364b96e0e14e28e63540a17514298a6a6
Samer Saied <samerpasha@hotmail.com>
2022-07-19 06:16:40 +0200

    Add Ads

--- lib/core/ads/ad_helper.dart
+++ lib/core/ads/ad_helper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 // import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class AdHelper {
-  static const bool _testMode = true;
+  static const bool _testMode = false;
 
   static String get appId {
     if (_testMode) {
@@ -32,25 +32,4 @@ class AdHelper {
       }
     }
   }
-
-  // static String get interstitialAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>";
-  //   } else if (Platform.isIOS) {
-  //     return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
-  //   } else {
-  //     throw UnsupportedError("Unsupported platform");
-  //   }
-  // }
-
-  // static String get rewardedAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
-  //   } else if (Platform.isIOS) {
-  //     return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
-  //   } else {
-  //     throw UnsupportedError("Unsupported platform");
-  //   }
-  // }
-
 }
