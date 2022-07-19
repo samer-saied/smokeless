ebf53b5dc723ce46cb70d8246842effc06c745e2
Samer Saied <samerpasha@hotmail.com>
2022-07-19 06:23:30 +0200

    add Ads

--- lib/core/ads/ad_banner_widget.dart
+++ lib/core/ads/ad_banner_widget.dart
@@ -1,69 +1,69 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:flutter/material.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
+// import 'package:flutter/material.dart';
+// import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-import 'ad_helper.dart';
+// import 'ad_helper.dart';
 
-class AddBannerWidget extends StatefulWidget {
-  const AddBannerWidget({Key? key}) : super(key: key);
+// class AddBannerWidget extends StatefulWidget {
+//   const AddBannerWidget({Key? key}) : super(key: key);
 
-  @override
-  State<AddBannerWidget> createState() => _AddBannerWidgetState();
-}
+//   @override
+//   State<AddBannerWidget> createState() => _AddBannerWidgetState();
+// }
 
-class _AddBannerWidgetState extends State<AddBannerWidget> {
-  ///
-  ///
-  ///
-  late BannerAd _bannerAd;
-  bool _isBannerAdReady = false;
-  final AdSize _adSize = AdSize.banner;
+// class _AddBannerWidgetState extends State<AddBannerWidget> {
+//   ///
+//   ///
+//   ///
+//   late BannerAd _bannerAd;
+//   bool _isBannerAdReady = false;
+//   final AdSize _adSize = AdSize.banner;
 
-  @override
-  void initState() {
-    _bannerAd = BannerAd(
-      adUnitId: AdHelper.interstitialAdUnitId,
-      request: const AdRequest(),
-      size: _adSize,
-      listener: BannerAdListener(
-        onAdLoaded: (_) {
-          setState(() {
-            _isBannerAdReady = true;
-          });
-        },
-        onAdFailedToLoad: (ad, err) {
-          log('Failed to load a banner ad: ${err.message}');
-          _isBannerAdReady = false;
-          ad.dispose();
-        },
-      ),
-    );
+//   @override
+//   void initState() {
+//     _bannerAd = BannerAd(
+//       adUnitId: AdHelper.interstitialAdUnitId,
+//       request: const AdRequest(),
+//       size: _adSize,
+//       listener: BannerAdListener(
+//         onAdLoaded: (_) {
+//           setState(() {
+//             _isBannerAdReady = true;
+//           });
+//         },
+//         onAdFailedToLoad: (ad, err) {
+//           log('Failed to load a banner ad: ${err.message}');
+//           _isBannerAdReady = false;
+//           ad.dispose();
+//         },
+//       ),
+//     );
 
-    _bannerAd.load();
-    super.initState();
-  }
+//     _bannerAd.load();
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    _bannerAd.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _bannerAd.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-        height: _adSize.height.toDouble(),
-        width: _adSize.width.toDouble(),
-        child: (_isBannerAdReady)
-            ? Align(
-                alignment: Alignment.topCenter,
-                child: SizedBox(
-                  width: _bannerAd.size.width.toDouble(),
-                  height: _bannerAd.size.height.toDouble(),
-                  child: AdWidget(ad: _bannerAd),
-                ),
-              )
-            : const SizedBox());
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//         height: _adSize.height.toDouble(),
+//         width: _adSize.width.toDouble(),
+//         child: (_isBannerAdReady)
+//             ? Align(
+//                 alignment: Alignment.topCenter,
+//                 child: SizedBox(
+//                   width: _bannerAd.size.width.toDouble(),
+//                   height: _bannerAd.size.height.toDouble(),
+//                   child: AdWidget(ad: _bannerAd),
+//                 ),
+//               )
+//             : const SizedBox());
+//   }
+// }
